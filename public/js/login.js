2dddd76502cff4bf45fe1ec02eb47102be5b0d37
gard971 <46375826+fuckNsuck@users.noreply.github.com>
2020-12-21 16:47:52 +0100

    moved inline css and js to external

---
+++ public/js/login.js
@@ -0,0 +1,46 @@
+var socket = io()
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log(document.getElementById("username").value)
+    socket.emit("login", document.getElementById("username").value, document.getElementById("password").value)
+})
+socket.on("passwordWrong", () => {
+    document.getElementById("wrongP").hidden = false
+})
+socket.on("passwordCorrect", (username, key) => {
+    if(document.getElementById("rememberMe").checked){
+        localStorage.setItem("username", username)
+        localStorage.setItem("key", key)
+    }
+    else{
+        sessionStorage.setItem("username", username)
+        sessionStorage.setItem("key", key)
+    }
+    var searchParams = window.location.search
+    var urlParams = new URLSearchParams(searchParams)
+    if(urlParams.get("redirect")){
+        if(urlParams.get("redirect") == "newAdress"){
+            newAdress()
+        }
+        else if(urlParams.get("redirect")){
+            window.location.href=urlParams.get("redirect")
+        }
+        else{
+            window.location.href="/"
+        }
+    }
+    else{
+        window.location.href="index.html"
+    }
+})
+socket.on("allowed", () => {
+    window.location.href="index.html"
+});
+(function(){
+    if(localStorage.getItem("username") && localStorage.getItem("key")){
+        socket.emit("check", localStorage.getItem("username"), localStorage.getItem("key"))
+    }
+    else if(sessionStorage.getItem("username") && sessionStorage.getItem("key")){
+        socket.emit("check", sessionStorage.getItem("username"), sessionStorage.getItem("key"))
+    }
+})()
---
+++ public/js/registrer.js
@@ -0,0 +1,15 @@
+var socket = io()
+document.getElementById("registrerForm").addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log(document.getElementById("username").value)
+    if (document.getElementById("password").value == document.getElementById("password2").value) {
+        socket.emit("register", document.getElementById("email").value, document.getElementById("username"), document.getElementById("password").value, )
+    }
+})
+socket.on("userCreated", () => {
+    alert("bruker opprettet")
+    window.location.href="nyadresse.html"
+})
+socket.on("usernameExists", () => {
+    document.getElementById("wrongP").hidden = false
+})
